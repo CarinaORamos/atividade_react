fb0c43d2e929468d188a297ce56c2d88954d8331
Carina O Ramos <carinaoliveira.ramos@gmail.com>
2024-05-29 22:17:07 -0300

    Altera cor e texto do HEADER

--- src/components/header.js
+++ src/components/header.js
@@ -5,23 +5,22 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-//import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
  
 function Header() {
     return (
         <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
+        <AppBar color= "warning" position="static">
           <Toolbar>
             <IconButton
               size="large"
               edge="start"
-              color="inherit"
               aria-label="menu"
               sx={{ mr: 2 }}
             >
-              {/*  
+              {  
               <MenuIcon />
-              */}
+              }
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Planejamento
